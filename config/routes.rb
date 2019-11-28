4c021e321d45efd957c95983a21e6c8d330de056
hiro0214 <leonhurt2@gmail.com>
2019-11-28 14:58:18 +0900

    route-fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :foods do
+    collection do
+      get "search"
+      get "more"
+      get "top"
+    end
     resources :good, only: [:create, :destroy]
   end
 
   resources :user, only: [:show] do
     get "good" => "user#good"
   end
-
-  get "search" => "foods#search"
-  get "more" => "foods#more"
-  get "top" => "foods#top"
 
 end
